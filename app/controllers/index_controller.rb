d9542db04b447748cafc318d3f6671f4b36e90c4
Michal Wong <contact.michaelwong@gmail.com>
2015-08-24 16:28:06 -0700

    add Geolocation in view

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,9 @@
+class IndexController < ApplicationController
+  def location
+  	latitude = params[:latitude]
+  	longitude = params[:longitude]
+
+  	#@voter = Voter.new()
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'index/location'
+  root 'index#location'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
